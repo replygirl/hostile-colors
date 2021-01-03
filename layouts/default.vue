30bb45ea875e3f8b68d381582986fce5f2222c40
replygirl <moon@moon.fyi>
2021-01-03 16:15:59 -0500

    don't use vh height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  Nuxt.min-h-screen
+  Nuxt.h-full
 </template>
 
 <style>
   html { @apply bg-black overflow-hidden uppercase; }
+  html, body, #__nuxt, #__layout { @apply h-full; }
   body { font: 12px/15px 'IBM Plex Mono' }
   button { @apply uppercase; }
 </style>
